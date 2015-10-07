33ffdc9bc09648e1da1dd4d18b2295d948e1f868
pheei <phe@gwu.edu>
2015-10-07 18:56:53 -0400

    layout update

--- app/src/main/java/com/example/hpishepei/weatherapp/activity/WeatherPageActivity.java
+++ app/src/main/java/com/example/hpishepei/weatherapp/activity/WeatherPageActivity.java
@@ -2,21 +2,17 @@ package com.example.hpishepei.weatherapp.activity;
 
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
-import android.widget.TextView;
 
 import com.example.hpishepei.weatherapp.R;
 
 public class WeatherPageActivity extends AppCompatActivity {
 
-    TextView location;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_weather_page);
-
-        location = (TextView)findViewById(R.id.location);
-        location.setText("dc");
 
 
     }
--- app/src/main/java/com/example/hpishepei/weatherapp/asynctask/LocationAsyncTask.java
+++ app/src/main/java/com/example/hpishepei/weatherapp/asynctask/LocationAsyncTask.java
@@ -1,13 +1,63 @@
 package com.example.hpishepei.weatherapp.asynctask;
 
+import android.Manifest;
+import android.content.Context;
+import android.content.pm.PackageManager;
+import android.location.Location;
+import android.location.LocationListener;
+import android.location.LocationManager;
 import android.os.AsyncTask;
+import android.os.Bundle;
+import android.util.Log;
+
+import com.example.hpishepei.weatherapp.Constants;
+
+import static android.support.v4.content.PermissionChecker.checkSelfPermission;
 
 /**
  * Created by hpishepei on 10/7/15.
  */
-public class LocationAsyncTask extends AsyncTask {
+public class LocationAsyncTask extends AsyncTask<String, Integer, String> {
+
+    private LocationManager mLocationManager;
+    private Context mContext;
+
+
+    public LocationAsyncTask(Context context){
+        mContext = context;
+        mLocationManager = (LocationManager)context.getSystemService(Context.LOCATION_SERVICE);
+    }
+
+
     @Override
-    protected Object doInBackground(Object[] params) {
-        return null;
+    protected String doInBackground(String... params) {
+
+
+        LocationListener locationListener = new LocationListener() {
+            public void onLocationChanged(Location location) {
+                // Called when a new location is found by the network location provider.
+                Log.i("Message: ", "Location changed, " + location.getAccuracy() + " , " + location.getLatitude() + "," + location.getLongitude());
+            }
+
+            public void onStatusChanged(String provider, int status, Bundle extras) {
+            }
+
+            public void onProviderEnabled(String provider) {
+            }
+
+            public void onProviderDisabled(String provider) {
+            }
+        };
+
+        if (checkSelfPermission(mContext, Manifest.permission.ACCESS_FINE_LOCATION) != PackageManager.PERMISSION_GRANTED){
+            return null;
+        }
+
+
+        mLocationManager.requestLocationUpdates(LocationManager.NETWORK_PROVIDER, Constants.TWENTY_MINUTES, 0, locationListener);
+
+        String currentLocation = mLocationManager.NETWORK_PROVIDER;
+
+        return currentLocation;
     }
 }
