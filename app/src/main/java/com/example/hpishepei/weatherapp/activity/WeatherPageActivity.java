32c74ccef9c1d67ee5a50316f5b09a37af7a3c12
pheei <phe@gwu.edu>
2015-10-13 01:02:21 -0400

    Location Function Finished
    Included location fetching class from professor

--- app/src/main/java/com/example/hpishepei/weatherapp/activity/WeatherPageActivity.java
+++ app/src/main/java/com/example/hpishepei/weatherapp/activity/WeatherPageActivity.java
@@ -1,22 +1,14 @@
 package com.example.hpishepei.weatherapp.activity;
 
-import android.app.ProgressDialog;
-import android.content.Context;
-import android.content.DialogInterface;
 import android.content.Intent;
 import android.location.Location;
-import android.location.LocationListener;
-import android.location.LocationManager;
-import android.os.AsyncTask;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
-import android.util.Log;
 import android.view.Menu;
 import android.view.MenuItem;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.ArrayAdapter;
-import android.widget.Button;
 import android.widget.ImageView;
 import android.widget.ListView;
 import android.widget.TextView;
@@ -24,26 +16,24 @@ import android.widget.Toast;
 
 import com.example.hpishepei.weatherapp.ChangePreferences;
 import com.example.hpishepei.weatherapp.R;
-import com.example.hpishepei.weatherapp.asynctask.LocationAsyncTask;
-import com.example.hpishepei.weatherapp.function.GetLocationInfo;
+import com.example.hpishepei.weatherapp.function.LocationFinder;
 import com.example.hpishepei.weatherapp.model.Weather;
 import com.example.hpishepei.weatherapp.model.WeatherList;
 
 import java.util.ArrayList;
 
-public class WeatherPageActivity extends AppCompatActivity implements LocationAsyncTask.LocationUpdateListener{
+public class WeatherPageActivity extends AppCompatActivity implements LocationFinder.LocationDetector {
 
     public static String NotationFlag;
     public static Double sLongitude = 0.0;
     public static Double sLatitude = 0.0;
-    //public static Boolean AutoLocation;
 
     private TextView mCurrentCityTextView;
     private TextView mCurrentWeather;
     private TextView mCurrentTemp;
     private TextView mTodaySummery;
     private ListView mListView;
-    private ImageView mRefreshButton;
+
 
 
     ArrayList<Weather> mWeatherList;
@@ -52,22 +42,7 @@ public class WeatherPageActivity extends AppCompatActivity implements LocationAs
 
     @Override
     protected void onResume() {
-        /**
-        Log.i("lll","onResume!!!!!!");
 
-        preferences = new ChangePreferences(this);
-        NotationFlag = preferences.getNotationSetting();
-        Log.i("ccc",WeatherPageActivity.NotationFlag);
-        //notifyAll();
-
-        mTodaySummery = (TextView)findViewById(R.id.today_summery);
-        if (NotationFlag.equals("C")){
-            mTodaySummery.setText(mWeatherList.get(0).getmSummeryC());
-        }
-        else if (NotationFlag.equals("F")){
-            mTodaySummery.setText(mWeatherList.get(0).getmSummeryF());
-        }
-*/
         super.onResume();
         updateView();
 
@@ -84,6 +59,8 @@ public class WeatherPageActivity extends AppCompatActivity implements LocationAs
         updateView();
 
     }
+
+
 
     private class WeatherListAdapter extends ArrayAdapter<Weather>{
         public WeatherListAdapter(ArrayList<Weather> mWeatherList){
@@ -141,6 +118,12 @@ public class WeatherPageActivity extends AppCompatActivity implements LocationAs
             startActivity(i);
             return true;
         }
+        else if (id == R.id.refresh_button){
+            LocationFinder locationFinder = new LocationFinder(WeatherPageActivity.this,WeatherPageActivity.this);
+            locationFinder.detectLocation();
+            return true;
+
+        }
 
         return super.onOptionsItemSelected(item);
     }
@@ -151,10 +134,6 @@ public class WeatherPageActivity extends AppCompatActivity implements LocationAs
     public void updateView(){
         preferences = new ChangePreferences(this);
         NotationFlag = preferences.getNotationSetting();
-
-        //SharedPreferences sharedpreferences = getSharedPreferences(Constants.My_PREFERENCES, Context.MODE_PRIVATE);
-        //NotationFlag = sharedpreferences.getString(Constants.NOTATION_SETTING, "C");
-        //AutoLocation = sharedpreferences.getBoolean(Constants.AUTOLOCATION_SETTING, true);
 
 
 
@@ -193,76 +172,8 @@ public class WeatherPageActivity extends AppCompatActivity implements LocationAs
         WeatherListAdapter adapter = new WeatherListAdapter(mWeatherList);
         mListView.setAdapter(adapter);
 
-        //GetLocationInfo getLocationInfo = new GetLocationInfo(this);
-        //getLocationInfo.FetchCordinates();
-
-        //GPSTracker gpsTracker = new GPSTracker(this);
 
 
-
-        //FetchCordinates fetchCordinates = new FetchCordinates();
-        //fetchCordinates.execute();
-
-
-        mRefreshButton = (ImageView)findViewById(R.id.refresh_Button);
-        mRefreshButton.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                GetLocationInfo getLocationInfo = new GetLocationInfo(WeatherPageActivity.this);
-                getLocationInfo.FetchCordinates();
-            }
-        });
-
-
-        //LocationAsyncTask asyncTask = new LocationAsyncTask(this);
-        //asyncTask.setmLocationUpdateListener(this);
-        //asyncTask.execute();
-
-/**
-        LocationManager mLocationManager = (LocationManager) this.getSystemService(Context.LOCATION_SERVICE);
-        //String provider = mLocationManager.getBestProvider(criteria, false);
-
-        mLocationManager.requestLocationUpdates(LocationManager.GPS_PROVIDER, 0, 0, new LocationListener() {
-            @Override
-            public void onLocationChanged(Location location) {
-                Log.i("lll","11111111111");
-
-                String latitude = Double.toString(location.getLatitude());
-                String longtitude = Double.toString(location.getLongitude());
-                Log.i("lll","New Location: "+"latitude:"+latitude+" longtitude:"+longtitude);
-            }
-
-            @Override
-            public void onStatusChanged(String provider, int status, Bundle extras) {
-
-            }
-
-            @Override
-            public void onProviderEnabled(String provider) {
-
-            }
-
-            @Override
-            public void onProviderDisabled(String provider) {
-
-            }
-        });
-
-
-
-
-        Location oldLocation = mLocationManager.getLastKnownLocation(LocationManager.NETWORK_PROVIDER);
-        if (oldLocation!=null){
-            String latitude = Double.toString(oldLocation.getLatitude());
-            String longtitude = Double.toString(oldLocation.getLongitude());
-            Log.i("lll","Old Location: "+"latitude:"+latitude+" longtitude:"+longtitude);
-        }
-
-
-
-    }
-
- */
 
 
 
@@ -270,122 +181,21 @@ public class WeatherPageActivity extends AppCompatActivity implements LocationAs
     }
 
 
-
-    public class FetchCordinates extends AsyncTask<String, Integer, String> {
-        ProgressDialog progDailog = null;
-
-        public double lati = 0.0;
-        public double longi = 0.0;
-
-        public LocationManager mLocationManager;
-        public VeggsterLocationListener mVeggsterLocationListener;
-
-        @Override
-        protected void onPreExecute() {
-            mVeggsterLocationListener = new VeggsterLocationListener();
-            mLocationManager = (LocationManager) getSystemService(Context.LOCATION_SERVICE);
-
-            mLocationManager.requestLocationUpdates(
-                    LocationManager.NETWORK_PROVIDER, 0, 0,
-                    mVeggsterLocationListener);
-
-            progDailog = new ProgressDialog(WeatherPageActivity.this);
-            progDailog.setOnCancelListener(new DialogInterface.OnCancelListener() {
-                @Override
-                public void onCancel(DialogInterface dialog) {
-                    FetchCordinates.this.cancel(true);
-                }
-            });
-            progDailog.setMessage("Loading...");
-            progDailog.setIndeterminate(true);
-            progDailog.setCancelable(true);
-            progDailog.show();
-
-        }
-
-        @Override
-        protected void onCancelled(){
-            System.out.println("Cancelled by user!");
-            progDailog.dismiss();
-            mLocationManager.removeUpdates(mVeggsterLocationListener);
-        }
-
-        @Override
-        protected void onPostExecute(String result) {
-            progDailog.dismiss();
-
-            Toast.makeText(WeatherPageActivity.this,
-                    "LATITUDE :" + lati + " LONGITUDE :" + longi,
-                    Toast.LENGTH_LONG).show();
-        }
-
-        @Override
-        protected String doInBackground(String... params) {
-            // TODO Auto-generated method stub
-
-            while (this.lati == 0.0) {
-
-            }
-            return null;
-        }
-
-        public class VeggsterLocationListener implements LocationListener {
-
-            @Override
-            public void onLocationChanged(Location location) {
-
-                int lat = (int) location.getLatitude(); // * 1E6);
-                int log = (int) location.getLongitude(); // * 1E6);
-                int acc = (int) (location.getAccuracy());
-
-                String info = location.getProvider();
-                try {
-
-                    // LocatorService.myLatitude=location.getLatitude();
-
-                    // LocatorService.myLongitude=location.getLongitude();
-
-                    lati = location.getLatitude();
-                    longi = location.getLongitude();
-
-                } catch (Exception e) {
-                    // progDailog.dismiss();
-                    // Toast.makeText(getApplicationContext(),"Unable to get Location"
-                    // , Toast.LENGTH_LONG).show();
-                }
-
-            }
-
-            @Override
-            public void onProviderDisabled(String provider) {
-                Log.i("OnProviderDisabled", "OnProviderDisabled");
-            }
-
-            @Override
-            public void onProviderEnabled(String provider) {
-                Log.i("onProviderEnabled", "onProviderEnabled");
-            }
-
-            @Override
-            public void onStatusChanged(String provider, int status,
-                                        Bundle extras) {
-                Log.i("onStatusChanged", "onStatusChanged");
-
-            }
-
-        }
-
-    }
-
-     
     @Override
-    public void LocationUpdated(String location) {
+    public void locationFound(Location location) {
+        sLongitude = location.getLongitude();
+        sLatitude = location.getLatitude();
+        Toast.makeText(this,("Longitude:"+sLongitude+", Latitude:"+sLatitude),Toast.LENGTH_SHORT).show();
 
     }
 
     @Override
-    public void LocationUpdateFail() {
+    public void locationNotFound(LocationFinder.FailureReason failureReason) {
+        Toast.makeText(this,this.getString(R.string.fail_label),Toast.LENGTH_SHORT).show();
+
 
     }
+
+
 
 }
---
+++ app/src/main/java/com/example/hpishepei/weatherapp/function/LocationFinder.java
@@ -0,0 +1,149 @@
+package com.example.hpishepei.weatherapp.function;
+
+import android.Manifest;
+import android.app.ProgressDialog;
+import android.content.Context;
+import android.content.DialogInterface;
+import android.content.pm.PackageManager;
+import android.location.Location;
+import android.location.LocationListener;
+import android.location.LocationManager;
+import android.os.Build;
+import android.os.Bundle;
+import android.os.Handler;
+import android.support.v4.content.ContextCompat;
+import android.util.Log;
+import android.widget.Toast;
+
+import com.example.hpishepei.weatherapp.R;
+
+/**
+ * Created by hpishepei on 10/13/15.
+ */
+public class LocationFinder implements LocationListener {
+    private static final String TAG = "LocationFinder";
+    private Context mContext;
+    private LocationDetector mLocationDetector;
+    private ProgressDialog mProgressDialog;
+
+    private final int TIMEOUT_IN_MS = 10000; //10 second timeout
+
+    private boolean mIsDetectingLocation = false;
+
+    //built into Android
+    private LocationManager mLocationManager;
+
+    public enum FailureReason{
+        NO_PERMISSION,
+        TIMEOUT
+    }
+
+    public interface LocationDetector{
+        void locationFound(Location location);
+        void locationNotFound(FailureReason failureReason);
+    }
+
+    public LocationFinder(Context context, LocationDetector locationDetector){
+        mContext = context;
+        mLocationDetector = locationDetector;
+    }
+
+    public void detectLocation(){
+        if(mIsDetectingLocation == false){
+            mIsDetectingLocation = true;
+
+            mProgressDialog = new ProgressDialog(mContext);
+            mProgressDialog.setIndeterminate(true);
+            mProgressDialog.setCancelable(true);
+            mProgressDialog.setMessage(mContext.getString(R.string.fetch_location_label));
+            mProgressDialog.setOnCancelListener(new DialogInterface.OnCancelListener() {
+                @Override
+                public void onCancel(DialogInterface dialog) {
+                    mProgressDialog.dismiss();
+                    Toast.makeText(mContext, mContext.getString(R.string.location_update_cancel_label), Toast.LENGTH_SHORT).show();
+                    endLocationDetection();
+                }
+            });
+            mProgressDialog.show();
+
+            if(mLocationManager == null){
+                mLocationManager = (LocationManager) mContext.getSystemService(Context.LOCATION_SERVICE);
+            }
+
+
+            if(ContextCompat.checkSelfPermission(mContext, Manifest.permission.ACCESS_COARSE_LOCATION) == PackageManager.PERMISSION_GRANTED || Build.VERSION.SDK_INT < 23) {
+                mLocationManager.requestSingleUpdate(LocationManager.NETWORK_PROVIDER, this, null);
+                startTimer();
+            }
+            else {
+                endLocationDetection();
+                mLocationDetector.locationNotFound(FailureReason.NO_PERMISSION);
+            }
+        }
+        else{
+            Log.d(TAG, "already trying to detect location");
+        }
+    }
+
+    private void endLocationDetection(){
+        if(mIsDetectingLocation) {
+            mProgressDialog.dismiss();
+            mIsDetectingLocation = false;
+
+            if(ContextCompat.checkSelfPermission(mContext, Manifest.permission.ACCESS_COARSE_LOCATION) == PackageManager.PERMISSION_GRANTED || Build.VERSION.SDK_INT < 23) {
+                mLocationManager.removeUpdates(this);
+            }
+        }
+    }
+
+    private void startTimer(){
+        Handler handler = new Handler();
+        handler.postDelayed(new Runnable() {
+            @Override
+            public void run() {
+                if(mIsDetectingLocation){
+                    mProgressDialog.dismiss();
+                    fallbackOnLastKnownLocation();
+                }
+            }
+        }, TIMEOUT_IN_MS);
+
+    }
+
+    private void fallbackOnLastKnownLocation(){
+        Location lastKnownLocation = null;
+
+        if(ContextCompat.checkSelfPermission(mContext, Manifest.permission.ACCESS_COARSE_LOCATION) == PackageManager.PERMISSION_GRANTED || Build.VERSION.SDK_INT < 23) {
+            lastKnownLocation = mLocationManager.getLastKnownLocation(LocationManager.NETWORK_PROVIDER);
+        }
+
+        if(lastKnownLocation != null){
+            mLocationDetector.locationFound(lastKnownLocation);
+        }
+        else{
+            mLocationDetector.locationNotFound(FailureReason.TIMEOUT);
+        }
+    }
+
+    @Override
+    public void onLocationChanged(Location location) {
+        mProgressDialog.dismiss();
+        mLocationDetector.locationFound(location);
+    }
+
+    @Override
+    public void onStatusChanged(String s, int i, Bundle bundle) {
+
+    }
+
+    @Override
+    public void onProviderEnabled(String s) {
+
+    }
+
+    @Override
+    public void onProviderDisabled(String s) {
+
+    }
+
+}
